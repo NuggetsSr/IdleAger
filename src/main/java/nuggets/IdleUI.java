41c542207b93b83310b92303a9af4711ba27561a
NuggetsSr <ericcai1219@gmail.com>
2026-04-24 15:16:30 -0400

    Window Listener Added, Shop Button Broken

--- src/main/java/nuggets/IdleUI.java
+++ src/main/java/nuggets/IdleUI.java
@@ -7,6 +7,8 @@ import java.awt.FlowLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -20,7 +22,7 @@ import javax.swing.SwingConstants;
 import javax.swing.Timer;
 
 public class IdleUI{
-    private static JFrame Mframe = new JFrame("Tracker");
+    private static final JFrame Mframe = new JFrame("Tracker");
     private static JFrame Gframe = new JFrame("Game");
     private static JFrame Sframe = new JFrame("Shop");
     private static JFrame popFrame = new JFrame("Warning"); // frame for the option panel 
@@ -28,12 +30,13 @@ public class IdleUI{
     private static JLabel UItimer = new JLabel("",SwingConstants.CENTER);
     private static JLabel UIUncCoin = new JLabel("",SwingConstants.CENTER); // curency earned from typing
     private static JLabel UIGUncCoin = new JLabel("",SwingConstants.CENTER);
+    private static JLabel up1cost = new JLabel("Up 1 Cost: " + Double.toString(IdleShop.getItemCost(0)));
+    private static JLabel up2cost = new JLabel("Up 2 Cost: " + Double.toString(IdleShop.getItemCost(1)));
+    private static JLabel up3cost = new JLabel("Up 3 Cost: " + Double.toString(IdleShop.getItemCost(2)));
     private static JPanel panel = new JPanel();
     private static ImageIcon icon = new ImageIcon("/Users/yifancai/Documents/IdleAger/image.png");
     private static JLabel animation = new JLabel(icon);
     private static JButton sButton = new JButton("Unpause");
-    private static JButton gButton = new JButton("Switch Game State");
-    private static JButton tButton = new JButton("Switch Track State");
     private static JButton EnterSButton = new JButton("Open Shop");
     private static JButton ExitSButton = new JButton("Exit Shop");
     private static JButton up1Button = new JButton("Decrease Quota");
@@ -43,6 +46,7 @@ public class IdleUI{
     private static int aniFrame = -240; // amount of pixels to shift animation by 
     private static final int tickAmount = 16; // framerate
     private static int frameCounter = 0;
+    private static boolean inFocus = true;
     
 
     private static String formatTime(int s){
@@ -57,7 +61,11 @@ public class IdleUI{
             UIcharCount.setText("Letters Typed: " + Integer.toString(UIdata.charCount));
             UItimer.setText("Time Left: " + formatTime(UIdata.focusTimer/1000));
             UIUncCoin.setText("Unc Coin Earned: " + Double.toString(UIdata.charCurrency));
-            sButton.setText((IdleAger.getPause() ? "Pause":"Unpause"));
+            sButton.setText((IdleAger.getPause() ? "Unpause":"Pause"));
+
+            up1cost.setText("Up 1 Cost: " + Double.toString(IdleShop.getItemCost(0))); 
+            up2cost.setText("Up 2 Cost: " + Double.toString(IdleShop.getItemCost(1))); 
+            up3cost.setText("Up 3 Cost: " + Double.toString(IdleShop.getItemCost(2))); 
         // }
     }
 
@@ -92,37 +100,70 @@ public class IdleUI{
 
     public static void init(){ // initializes all the UI
         GlobalKeyListenerExample.init();
-        // set an actionlistener in main, add it here
+        // Init GlobalKeyListener
 
-        JLabel up1cost = new JLabel("Up 1 Cost: " + Double.toString(IdleShop.getItemCost(0)));
-        JLabel up2cost = new JLabel("Up 2 Cost: " + Double.toString(IdleShop.getItemCost(1)));
-        JLabel up3cost = new JLabel("Up 3 Cost: " + Double.toString(IdleShop.getItemCost(2)));
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) size.getWidth();
+        int length = (int) size.getHeight();
+        // get dimension of screen
+
+        //main frame
+        Mframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Mframe.setSize(240,240);
+        Mframe.setLocation(width-240, length-240);
+        Mframe.setAlwaysOnTop(true);
+        Mframe.setResizable(false);
+
+        //shop frame
+        Sframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Sframe.setSize(240,240);
+        Sframe.setLocation(width-240, length-240);
+        Sframe.setAlwaysOnTop(true);
+        Sframe.setResizable(false); 
+
+        //game frame
+        Gframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Gframe.setSize(240,240);
+        Gframe.setLocation(width-240, length-240);
+        Gframe.setAlwaysOnTop(true);
+        Gframe.setResizable(false);
+
+        Mframe.addWindowFocusListener(new WindowFocusListener(){
+            @Override
+            public void windowLostFocus(WindowEvent e){
+                stateMachine(true, false);
+            }
+
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                stateMachine(false, false);
+            }
+        });
+
+         Gframe.addWindowFocusListener(new WindowFocusListener(){
+            @Override
+            public void windowLostFocus(WindowEvent e){
+                
+            }
+
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                stateMachine(false, false);
+            }
+        });
+
         sButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 IdleAger.setPause();
             }
         });
-        gButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                stateMachine(true,false);
-            }
-        });
-        tButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                stateMachine(false, false);
-            }
-        });
 
         EnterSButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
+                inFocus = false;
                 stateMachine(false, true);
-                up1cost.setText("Up 1 Cost: " + Double.toString(IdleShop.getItemCost(0))); 
-                up2cost.setText("Up 2 Cost: " + Double.toString(IdleShop.getItemCost(1))); 
-                up3cost.setText("Up 3 Cost: " + Double.toString(IdleShop.getItemCost(2))); 
                 
 
             }
@@ -159,22 +200,6 @@ public class IdleUI{
 
         aniTimer.start();
 
-        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = (int) size.getWidth();
-        int length = (int) size.getHeight();
-        
-        Mframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Mframe.setSize(240,240);
-        Mframe.setLocation(width-240, length-240);
-        Mframe.setAlwaysOnTop(true);
-        Mframe.setResizable(false);
-
-        Sframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Sframe.setSize(240,240);
-        Sframe.setLocation(width-240, length-240);
-        Sframe.setAlwaysOnTop(true);
-        Sframe.setResizable(false); 
-
         JPanel SPanel = new JPanel();
         SPanel.setLayout(new BoxLayout(SPanel,BoxLayout.Y_AXIS));
         SPanel.add(ExitSButton);
@@ -188,14 +213,12 @@ public class IdleUI{
         Sframe.add(SPanel);
 
         animation.setBounds(0,-10,960,240);
-        tButton.setBounds(100, 50, 200, 200);
         
         //layers for Game animation and buttons
         JLayeredPane Glayers = new JLayeredPane();
         JPanel GPanel = new JPanel();
         Glayers.setLayout(new FlowLayout());
         GPanel.setLayout(new BorderLayout());
-        GPanel.add(tButton, BorderLayout.CENTER);
         UIGUncCoin.setForeground(new Color(255,255,255));
         GPanel.add(UIGUncCoin, BorderLayout.SOUTH);
         GPanel.setOpaque(false);
@@ -208,20 +231,11 @@ public class IdleUI{
         // Gframe.add(UIGcharCurrency);
         // Gframe.add(GPanel);
 
-        Gframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Gframe.setSize(240,240);
-        Gframe.setLocation(width-240, length-240);
-        Gframe.setAlwaysOnTop(true);
-        Gframe.setResizable(false);
-
-        popFrame.setSize(10000, 10000);
-        
         // button panel for buttons (start session, end session, switch to game)
         JPanel bPanel = new JPanel();
         bPanel.setLayout(new BoxLayout(bPanel, BoxLayout.Y_AXIS));
         bPanel.add(sButton);
         bPanel.add(EnterSButton);
-        bPanel.add(gButton);
         bPanel.setSize(240,240);
         panel.setLayout(new FlowLayout());
         panel.add(UIcharCount);
